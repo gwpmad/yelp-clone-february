e470e7dfea4a1e1caa6c541d62f9e7667efa8e37
gwpmad <georgemaddocks@yahoo.co.uk>
2016-02-25 09:53:45 +0000

    CRUD complete

--- yelp-clone-february/yelp_clone/app/controllers/restaurants_controller.rb
+++ yelp-clone-february/yelp_clone/app/controllers/restaurants_controller.rb
@@ -4,6 +4,7 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
@@ -11,7 +12,33 @@ class RestaurantsController < ApplicationController
     redirect_to '/restaurants'
   end
 
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+
+    redirect_to '/restaurants'
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    flash[:notice] = 'Restaurant deleted successfully' #referenced in application.html.erb
+    redirect_to '/restaurants'
+  end
+
   def restaurant_params
     params.require(:restaurant).permit(:name)
+    # referencing the params hash created by the 'create' action - see
+    # new.html.erb - rails' require method finds checks that the hash has the
+    # passed-in key and raises an error if it doesn't.
+    # http://api.rubyonrails.org/classes/ActionController/Parameters.html#method-i-require
   end
 end
